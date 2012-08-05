da40856a01f2ae8ede7598c6e4b6cc0a1c7e8e89
dondi <dondi@lmu.edu>
2012-08-04 23:54:48 -0700

    Initial stub-out of client page template.

---
+++ client-webapp/src/main/java/edu/lmu/cs/headmaster/client/web/ClientPage.java
@@ -0,0 +1,29 @@
+package edu.lmu.cs.headmaster.client.web;
+
+import org.apache.wicket.PageParameters;
+import org.apache.wicket.authorization.strategies.role.Roles;
+import org.apache.wicket.authorization.strategies.role.annotations.AuthorizeInstantiation;
+import org.apache.wicket.markup.html.WebPage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * ClientPage is the superclass for all logged-in Headmaster views.
+ */
+@AuthorizeInstantiation(Roles.USER)
+public abstract class ClientPage extends WebPage {
+
+    public ClientPage(final PageParameters pageParameters) {
+        super(pageParameters);
+    }
+
+    /**
+     * Convenience method for grabbing the logger.  This is not stored as an
+     * instance variable because Wicket serializes web page classes and this
+     * logger is not serializable.
+     */
+    protected Logger getLogger() {
+        return LoggerFactory.getLogger(getClass());
+    }
+
+}
--- client-webapp/src/main/java/edu/lmu/cs/headmaster/client/web/MainMenuPage.java
+++ client-webapp/src/main/java/edu/lmu/cs/headmaster/client/web/MainMenuPage.java
@@ -1,15 +1,11 @@
 package edu.lmu.cs.headmaster.client.web;
 
 import org.apache.wicket.PageParameters;
-import org.apache.wicket.authorization.strategies.role.Roles;
-import org.apache.wicket.authorization.strategies.role.annotations.AuthorizeInstantiation;
-import org.apache.wicket.markup.html.WebPage;
 
 /**
  * Corresponding Java class for the main menu page.
  */
-@AuthorizeInstantiation(Roles.USER)
-public class MainMenuPage extends WebPage {
+public class MainMenuPage extends ClientPage {
 
     public MainMenuPage(final PageParameters pageParameters) {
         super(pageParameters);
