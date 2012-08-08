00f790c1359ea2f0d15fdba1cff98429270eab21
dondi <dondi@lmu.edu>
2012-08-08 16:19:19 -0700

    Added expected graduation year to the test database fixture.

--- service/src/test/resources/import.sql
+++ service/src/test/resources/import.sql
@@ -5,9 +5,9 @@ insert into userrole(id, rolename, login) values(1000000, 'HEADMASTER', 'admin')
 insert into serviceuser(id, active, challenge, email, login) values(1000001, true, 'password-noroles', 'noroles@headmaster.test', 'noroles');
 
 -- Preloaded students.
-insert into student(id, firstname, lastname) values(1000000, 'Tim', 'Berners-Lee');
-insert into student(id, firstname, lastname) values(1000001, 'Vint', 'Cerf');
-insert into student(id, firstname, lastname) values(1000002, 'Don', 'Knuth');
+insert into student(id, firstname, lastname, active, expectedgraduationyear) values(1000000, 'Tim', 'Berners-Lee', true, 2016);
+insert into student(id, firstname, lastname, active, expectedgraduationyear) values(1000001, 'Vint', 'Cerf', true, 2015);
+insert into student(id, firstname, lastname, active, expectedgraduationyear) values(1000002, 'Don', 'Knuth', true, 2015);
 
 -- Preloaded events.
 insert into event(id, datetime, description, title) values(1000000, '2012-07-28 10:31:03', 'The big one', 'Summit');
