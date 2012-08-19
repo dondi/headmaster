6ee7a6d3becd6876d00c4482459862bf22a5ff50
dondi <dondi@lmu.edu>
2012-08-19 15:35:28 -0700

    Stubbed out term service interface and wrote unit tests for them.

---
+++ service/src/main/java/edu/lmu/cs/headmaster/ws/service/TermService.java
@@ -0,0 +1,60 @@
+package edu.lmu.cs.headmaster.ws.service;
+
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * The JAX-RS service for accessing "terms" in the database---i.e.,
+ * currently-stored values for certain fields.
+ */
+@Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+public interface TermService {
+
+    /**
+     * Returns the currently-saved values for college or school that match the
+     * given query term.  Results are returned in alphabetical order.
+     * 
+     * @param query the query
+     * @param skip the number of initial results to skip
+     * @param max the maximum number of results to display
+     * 
+     * @return the (paginated) set of terms matching the query parameters;
+     *         400 query.required if q is not supplied
+     */
+    @GET
+    @Path("colleges-or-schools")
+    List<String> getMatchingCollegesOrSchools(@PathParam("q") String query,
+            @QueryParam("skip") @DefaultValue("0") int skip,
+            @QueryParam("max") @DefaultValue("50") int max);
+
+    /**
+     * Returns the currently-saved values for degree that match the given query
+     * term.  Results are returned in alphabetical order.
+     */
+    @GET
+    @Path("degrees")
+    List<String> getMatchingDegrees(@PathParam("q") String query,
+            @QueryParam("skip") @DefaultValue("0") int skip,
+            @QueryParam("max") @DefaultValue("50") int max);
+
+    /**
+     * Returns the currently-saved values for discipline (whether stored as a
+     * major or minor) that match the given query term.  Results are returned
+     * in alphabetical order.
+     */
+    @GET
+    @Path("disciplines")
+    List<String> getMatchingDisciplines(@PathParam("q") String query,
+            @QueryParam("skip") @DefaultValue("0") int skip,
+            @QueryParam("max") @DefaultValue("50") int max);
+
+}
---
+++ service/src/test/java/edu/lmu/cs/headmaster/ws/service/TermServiceTest.java
@@ -0,0 +1,157 @@
+package edu.lmu.cs.headmaster.ws.service;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.GenericType;
+
+/**
+ * Tests the term web service.
+ */
+public class TermServiceTest extends ServiceTest {
+
+    @Test
+    public void testGetMatchingCollegesOrSchoolsNoQuery() {
+        ClientResponse clientResponse = ws.path("terms/colleges-or-schools")
+                .get(ClientResponse.class);
+
+        // We expect error 400, QUERY_REQUIRED.
+        Assert.assertEquals(400, clientResponse.getStatus());
+        Assert.assertEquals(
+            "400 " + AbstractService.QUERY_REQUIRED,
+            clientResponse.getEntity(String.class)
+        );
+    }
+
+    @Test
+    public void testGetMatchingDegreesNoQuery() {
+        ClientResponse clientResponse = ws.path("terms/degrees")
+                .get(ClientResponse.class);
+
+        // We expect error 400, QUERY_REQUIRED.
+        Assert.assertEquals(400, clientResponse.getStatus());
+        Assert.assertEquals(
+            "400 " + AbstractService.QUERY_REQUIRED,
+            clientResponse.getEntity(String.class)
+        );
+    }
+
+    @Test
+    public void testGetMatchingDisciplinesNoQuery() {
+        ClientResponse clientResponse = ws.path("terms/disciplines")
+                .get(ClientResponse.class);
+
+        // We expect error 400, QUERY_REQUIRED.
+        Assert.assertEquals(400, clientResponse.getStatus());
+        Assert.assertEquals(
+            "400 " + AbstractService.QUERY_REQUIRED,
+            clientResponse.getEntity(String.class)
+        );
+    }
+
+    @Test
+    public void testGetMatchingCollegeOrSchool() {
+        // "en" should match both science and engineering.
+        List<String> results = ws.path("terms/colleges-or-schools")
+        		.queryParam("q", "en")
+        		.get(new GenericType<List<String>>() {});
+
+        Assert.assertEquals(2, results.size());
+        Assert.assertEquals("Engineering", results.get(0));
+        Assert.assertEquals("Science", results.get(1));
+
+        // "sc" should match only science.
+        results = ws.path("terms/colleges-or-schools")
+                .queryParam("q", "sc")
+                .get(new GenericType<List<String>>() {});
+
+        Assert.assertEquals(1, results.size());
+        Assert.assertEquals("Science", results.get(0));
+
+        // "eer" should match only engineering.
+        results = ws.path("terms/colleges-or-schools")
+                .queryParam("q", "eer")
+                .get(new GenericType<List<String>>() {});
+
+        Assert.assertEquals(1, results.size());
+        Assert.assertEquals("Engineering", results.get(0));
+
+        // "zatoichi" should match nothing.
+        results = ws.path("terms/colleges-or-schools")
+                .queryParam("q", "zatoichi")
+                .get(new GenericType<List<String>>() {});
+
+        Assert.assertEquals(0, results.size());
+    }
+
+    @Test
+    public void testGetMatchingDegree() {
+        // "b" should match both BA and BS.
+        List<String> results = ws.path("terms/degrees")
+                .queryParam("q", "b")
+                .get(new GenericType<List<String>>() {});
+
+        Assert.assertEquals(2, results.size());
+        Assert.assertEquals("BA", results.get(0));
+        Assert.assertEquals("BS", results.get(1));
+
+        // "s" should match only BS.
+        results = ws.path("terms/degrees")
+                .queryParam("q", "s")
+                .get(new GenericType<List<String>>() {});
+
+        Assert.assertEquals(1, results.size());
+        Assert.assertEquals("BS", results.get(0));
+
+        // "a" should match only BA.
+        results = ws.path("terms/degrees")
+                .queryParam("q", "a")
+                .get(new GenericType<List<String>>() {});
+
+        Assert.assertEquals(1, results.size());
+        Assert.assertEquals("BA", results.get(0));
+
+        // "bazinga" should match nothing.
+        results = ws.path("terms/degrees")
+                .queryParam("q", "bazinga")
+                .get(new GenericType<List<String>>() {});
+
+        Assert.assertEquals(0, results.size());
+    }
+
+    @Test
+    public void testGetMatchingDiscipline() {
+        // "ic" should match both Mathematics and Music.
+        List<String> results = ws.path("terms/degrees")
+                .queryParam("q", "ic")
+                .get(new GenericType<List<String>>() {});
+
+        Assert.assertEquals(2, results.size());
+        Assert.assertEquals("Mathematics", results.get(0));
+        Assert.assertEquals("Music", results.get(1));
+
+        // "comp" should match only Computer Science.
+        results = ws.path("terms/disciplines")
+                .queryParam("q", "comp")
+                .get(new GenericType<List<String>>() {});
+        Assert.assertEquals(1, results.size());
+        Assert.assertEquals("Computer Science", results.get(0));
+
+        // "us" should match only Music.
+        results = ws.path("terms/degrees")
+                .queryParam("q", "us")
+                .get(new GenericType<List<String>>() {});
+        Assert.assertEquals(1, results.size());
+        Assert.assertEquals("Music", results.get(0));
+
+        // "cucaracha" should match nothing.
+        results = ws.path("terms/degrees")
+                .queryParam("q", "cucaracha")
+                .get(new GenericType<List<String>>() {});
+        Assert.assertEquals(0, results.size());
+    }
+
+}
