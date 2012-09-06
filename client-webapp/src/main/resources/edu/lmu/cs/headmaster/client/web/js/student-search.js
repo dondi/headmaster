e6a4baaa6976e62ddbe10d39fd1c13a824a5247c
dondi <dondi@lmu.edu>
2012-09-06 11:51:41 -0700

    Added automatic triggering of search if the search field already has something in it upon loading.

--- client-webapp/src/main/resources/edu/lmu/cs/headmaster/client/web/js/student-search.js
+++ client-webapp/src/main/resources/edu/lmu/cs/headmaster/client/web/js/student-search.js
@@ -1,6 +1,9 @@
 $(function () {
+    var searchField = $("#search-field");
+
+    // Set up search field event handling.
     Headmaster.setupSearchField(
-        $("#search-field"), $("#search-progress"), $("#search-empty"), $("#search-results"),
+        searchField, $("#search-progress"), $("#search-empty"), $("#search-results"),
         Headmaster.serviceUri("students"), "q",
         function (student) {
             return $("<tr></tr>")
@@ -20,4 +23,9 @@ $(function () {
                 });
         }
     );
+
+    // If there is already a value in the search field, trigger a search.
+    if (searchField.val()) {
+        searchField.trigger("input");
+    }
 });
