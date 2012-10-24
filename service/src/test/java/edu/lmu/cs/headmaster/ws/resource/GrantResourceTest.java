1c96c8c2b44ff78286b08f822aceff0888f8b7a9
Tyler Nichols <tyler.n.89@gmail.com>
2012-10-23 18:25:59 -0700

    Added a few Grant resource unit tests.

--- service/src/test/java/edu/lmu/cs/headmaster/ws/resource/GrantResourceTest.java
+++ service/src/test/java/edu/lmu/cs/headmaster/ws/resource/GrantResourceTest.java
@@ -11,6 +11,7 @@ import com.sun.jersey.api.client.GenericType;
 import com.sun.jersey.api.client.UniformInterfaceException;
 
 import edu.lmu.cs.headmaster.ws.domain.Grant;
+import edu.lmu.cs.headmaster.ws.util.DomainObjectUtils;
 
 /**
  * Tests the user web resource.
@@ -39,12 +40,72 @@ public class GrantResourceTest extends ResourceTest {
     
     @Test
     public void testGetGrantsQueryByName() {
-        
         List<Grant> grants = wr.path("grants")
                 .queryParam("q", "leonard")
                 .get(new GenericType<List<Grant>>(){});
         
-        //List<Grant> grants = wr.path("grants?q=leonard").get(ClientResponse.class).getEntity(List.class);
         Assert.assertEquals(grants.size(), 1);
     }
+    
+    @Test
+    public void testGetGrantsQueryByTitle() {
+        List<Grant> grants = wr.path("grants")
+                .queryParam("q", "worldwide")
+                .get(new GenericType<List<Grant>>(){});
+        
+        Assert.assertEquals(grants.size(), 1);
+    }
+    
+    @Test
+    public void testCreateGrant() {
+        // Create an id-less event.
+        Grant grantToCreate = DomainObjectUtils.createGrantObject(
+            1024, "Tyler Nichols", "Marc Papkyriakou"
+        );
+
+        // Now, save it.  We should get a 201 with a location.
+        ClientResponse response = wr.path("grants").post(ClientResponse.class, grantToCreate);
+        Assert.assertEquals(201, response.getStatus());
+    }
+    
+    @Test
+    public void testCreateGrantWithSpecifiedIdProduces400() {
+        // Create an id-less event.
+        Grant grantToCreate = DomainObjectUtils.createGrantObject(
+            1024, "Tyler Nichols", "Marc Papkyriakou lol"
+        );
+        
+        grantToCreate.setId(500L);
+
+        // Now, save it.  We should get a 201 with a location.
+        ClientResponse response = wr.path("grants").post(ClientResponse.class, grantToCreate);
+        Assert.assertEquals(400, response.getStatus());
+    }
+    
+    @Test
+    public void testUpdateGrantProduces204() {
+        // Create an id-less event.
+        Grant grantToUpdate = DomainObjectUtils.createGrantObject(
+            1000, "Andy Won", "Won Grant"
+        );
+
+        grantToUpdate.setId(1000000L);
+        
+        // Now, save it.  We should get a 201 with a location.
+        ClientResponse response = wr.path("grants/1000000").put(ClientResponse.class, grantToUpdate);
+        Assert.assertEquals(204, response.getStatus());
+    }
+    
+    @Test
+    public void testUpdateGrantWithInconsistentInfoProduces400() {
+        // Create an id-less event.
+        Grant grantToUpdate = DomainObjectUtils.createGrantObject(
+            1000, "Andy Won", "Won Grant"
+        );
+
+        // Now, save it.  We should get a 201 with a location.
+        ClientResponse response = wr.path("grants/1000").put(ClientResponse.class, grantToUpdate);
+        Assert.assertEquals(400, response.getStatus());
+    }
+ 
 }
