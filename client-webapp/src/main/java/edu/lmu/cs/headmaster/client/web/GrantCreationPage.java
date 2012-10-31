3bf247aa836f77b885c5ec3e305c3d5ac9e69d65
Andrew Won <andyjwon@gmail.com>
2012-10-31 12:42:39 -0700

    Added GrantWriterPage and GrantCreationPage.java

---
+++ client-webapp/src/main/java/edu/lmu/cs/headmaster/client/web/GrantCreationPage.java
@@ -0,0 +1,11 @@
+package edu.lmu.cs.headmaster.client.web;
+
+import org.apache.wicket.PageParameters;
+
+public class GrantCreationPage extends GrantWriterPage {
+
+    public GrantCreationPage(final PageParameters pageParameters) {
+        super(pageParameters);
+    }
+
+}
