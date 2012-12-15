abe8af08c8b7eb4d6da48b1d3d9465e67bb6006e
sam <verhasseltsamuelj@gmail.com>
2012-12-14 18:21:06 -0800

    Refactored TermResource into TermResource and TermService layers.

---
+++ service/src/main/java/edu/lmu/cs/headmaster/ws/service/TermServiceImpl.java
@@ -0,0 +1,31 @@
+package edu.lmu.cs.headmaster.ws.service;
+
+import java.util.List;
+
+import edu.lmu.cs.headmaster.ws.dao.StudentDao;
+
+public class TermServiceImpl extends AbstractService implements TermService {
+
+    private StudentDao studentDao;
+
+    public TermServiceImpl(StudentDao studentDao) {
+        this.studentDao = studentDao;
+    }
+
+	@Override
+	public List<String> getMatchingCollegesOrSchools(String query, int skip,
+			int max) {
+		 return studentDao.getMatchingCollegesOrSchools(query, skip, max);
+	}
+
+	@Override
+	public List<String> getMatchingDegrees(String query, int skip, int max) {
+		  return studentDao.getMatchingDegrees(query, skip, max);
+	}
+
+	@Override
+	public List<String> getMatchingDisciplines(String query, int skip, int max) {
+        return studentDao.getMatchingDisciplines(query, skip, max);
+	}
+
+}
