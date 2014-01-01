972ffda096711d288a78d11f055835b7ed9a54da
dondi <dondi@lmu.edu>
2014-01-01 14:53:19 -0800

    Initial stub-out of course dao interface with unit test.

---
+++ service/src/main/java/edu/lmu/cs/headmaster/ws/dao/CourseDao.java
@@ -0,0 +1,33 @@
+package edu.lmu.cs.headmaster.ws.dao;
+
+import java.util.List;
+
+import edu.lmu.cs.headmaster.ws.domain.Course;
+
+/**
+ * Simple dao for course domain objects.
+ */
+public interface CourseDao {
+
+    /**
+     * Returns the course with the given id, or null if no such course exists.
+     */
+    Course getCourseById(Long id);
+
+    /**
+     * Returns a paginated set of courses that match the required query term,
+     * skipping the first <code>skip</code> results and returning at most
+     * <code>max</code> results.
+     */
+    List<Course> getCourses(String query, int skip, int year);
+
+    /**
+     * Saves the given course, which should have a null id.
+     */
+    Course createCourse(Course course);
+
+    /**
+     * Updates or saves the given course, which should have a non-null id.
+     */
+    void createOrUpdateCourse(Course course);
+}
---
+++ service/src/test/java/edu/lmu/cs/headmaster/ws/dao/CourseDaoTest.java
@@ -0,0 +1,77 @@
+package edu.lmu.cs.headmaster.ws.dao;
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.lmu.cs.headmaster.ws.domain.Course;
+import edu.lmu.cs.headmaster.ws.types.Term;
+import edu.lmu.cs.headmaster.ws.util.ApplicationContextTest;
+
+public class CourseDaoTest extends ApplicationContextTest {
+
+    private CourseDao courseDao;
+
+    @Before
+    public void getRequiredBeans() {
+        courseDao = (CourseDao)applicationContext.getBean("courseDao");
+    }
+
+    @Test
+    public void testGetCourseById() {
+        Assert.assertEquals(Long.valueOf(1000002L), courseDao.getCourseById(1000002L));
+        Assert.assertEquals(Long.valueOf(1000000L), courseDao.getCourseById(1000000L));
+        Assert.assertEquals(Long.valueOf(1000001L), courseDao.getCourseById(1000001L));
+    }
+
+    @Test
+    public void testCreateBasicCourse() {
+        // Create an id-less course.
+        Course courseToCreate = new Course();
+        courseToCreate.setNumber("486");
+        courseToCreate.setSection("01");
+        courseToCreate.setSubject("CMSI");
+        courseToCreate.setTerm(Term.FALL);
+        courseToCreate.setTitle("Databases");
+        courseToCreate.setYear(2020);
+
+        // Save the course.
+        courseDao.createCourse(courseToCreate);
+
+        // The created course should now have an ID. Because we know what is in
+        // the test fixture, we know what ID to expect.
+        Assert.assertEquals(Long.valueOf(1L), courseToCreate.getId());
+
+        // Reload the event that was just created.
+        Course createdCourse = courseDao.getCourseById(1L);
+        assertSimpleEquality(courseToCreate, createdCourse);
+        Assert.assertEquals(0, createdCourse.getObjectives().size());
+        Assert.assertEquals(0, createdCourse.getAssignments().size());
+        Assert.assertEquals(0, createdCourse.getStudents().size());
+    }
+
+    @Test
+    public void testCreateOrUpdateCourse() {
+        // Grab a course from the fixture.
+        Course course = courseDao.getCourseById(1000001L);
+        course.setSubject("MUSC");
+        courseDao.createOrUpdateCourse(course);
+        
+        Course updatedCourse = courseDao.getCourseById(1000001L);
+        assertSimpleEquality(course, updatedCourse);
+    }
+
+    /**
+     * Helper function for asserting the equality of two courses.
+     */
+    private void assertSimpleEquality(Course course1, Course course2) {
+        Assert.assertEquals(course1.getId(), course2.getId());
+        Assert.assertEquals(course1.getNumber(), course2.getNumber());
+        Assert.assertEquals(course1.getSection(), course2.getSection());
+        Assert.assertEquals(course1.getSubject(), course2.getSubject());
+        Assert.assertEquals(course1.getTerm(), course2.getTerm());
+        Assert.assertEquals(course1.getTitle(), course2.getTitle());
+        Assert.assertEquals(course1.getYear(), course2.getYear());
+    }
+}
