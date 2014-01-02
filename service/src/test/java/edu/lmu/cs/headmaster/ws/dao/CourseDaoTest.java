fde52507f326b9d15ccc882fd402dffddef03ebf
dondi <dondi@lmu.edu>
2014-01-01 16:33:57 -0800

    Refactor some common code into utilities class.

--- service/src/test/java/edu/lmu/cs/headmaster/ws/dao/CourseDaoTest.java
+++ service/src/test/java/edu/lmu/cs/headmaster/ws/dao/CourseDaoTest.java
@@ -10,6 +10,7 @@ import org.junit.Test;
 import edu.lmu.cs.headmaster.ws.domain.Course;
 import edu.lmu.cs.headmaster.ws.types.Term;
 import edu.lmu.cs.headmaster.ws.util.ApplicationContextTest;
+import edu.lmu.cs.headmaster.ws.util.DomainObjectUtils;
 
 public class CourseDaoTest extends ApplicationContextTest {
 
@@ -42,13 +43,7 @@ public class CourseDaoTest extends ApplicationContextTest {
     @Test
     public void testCreateBasicCourse() {
         // Create an id-less course.
-        Course courseToCreate = new Course();
-        courseToCreate.setNumber("486");
-        courseToCreate.setSection("01");
-        courseToCreate.setSubject("CMSI");
-        courseToCreate.setTerm(Term.FALL);
-        courseToCreate.setTitle("Databases");
-        courseToCreate.setYear(2020);
+        Course courseToCreate = DomainObjectUtils.createCourseObject("486", "01", "CMSI", Term.FALL, "Databases", 2020);
 
         // Save the course.
         courseDao.createCourse(courseToCreate);
@@ -59,7 +54,7 @@ public class CourseDaoTest extends ApplicationContextTest {
 
         // Reload the event that was just created.
         Course createdCourse = courseDao.getCourseById(1L);
-        assertSimpleEquality(courseToCreate, createdCourse);
+        DomainObjectUtils.assertSimpleEquality(courseToCreate, createdCourse);
         Assert.assertEquals(0, createdCourse.getObjectives().size());
         Assert.assertEquals(0, createdCourse.getAssignments().size());
         Assert.assertEquals(0, createdCourse.getStudents().size());
@@ -73,20 +68,7 @@ public class CourseDaoTest extends ApplicationContextTest {
         courseDao.createOrUpdateCourse(course);
         
         Course updatedCourse = courseDao.getCourseById(1000001L);
-        assertSimpleEquality(course, updatedCourse);
-    }
-
-    /**
-     * Helper function for asserting the equality of two courses.
-     */
-    private void assertSimpleEquality(Course course1, Course course2) {
-        Assert.assertEquals(course1.getId(), course2.getId());
-        Assert.assertEquals(course1.getNumber(), course2.getNumber());
-        Assert.assertEquals(course1.getSection(), course2.getSection());
-        Assert.assertEquals(course1.getSubject(), course2.getSubject());
-        Assert.assertEquals(course1.getTerm(), course2.getTerm());
-        Assert.assertEquals(course1.getTitle(), course2.getTitle());
-        Assert.assertEquals(course1.getYear(), course2.getYear());
+        DomainObjectUtils.assertSimpleEquality(course, updatedCourse);
     }
 
 }
--- service/src/test/java/edu/lmu/cs/headmaster/ws/util/DomainObjectUtils.java
+++ service/src/test/java/edu/lmu/cs/headmaster/ws/util/DomainObjectUtils.java
@@ -3,13 +3,17 @@ package edu.lmu.cs.headmaster.ws.util;
 import java.util.ArrayList;
 import java.util.List;
 
+import junit.framework.Assert;
+
 import org.joda.time.DateTime;
 
+import edu.lmu.cs.headmaster.ws.domain.Course;
 import edu.lmu.cs.headmaster.ws.domain.Grant;
 import edu.lmu.cs.headmaster.ws.domain.Event;
 import edu.lmu.cs.headmaster.ws.domain.User;
 import edu.lmu.cs.headmaster.ws.domain.UserRole;
 import edu.lmu.cs.headmaster.ws.types.Role;
+import edu.lmu.cs.headmaster.ws.types.Term;
 
 /**
  * Holder for utility methods of use to multiple unit tests.
@@ -57,6 +61,34 @@ public class DomainObjectUtils {
         event.setDescription(description);
         event.setDateTime(dateTime);
         return event;
+    }
+
+    /**
+     * Helper factory method for creating new course objects.
+     */
+    public static Course createCourseObject(String number, String section, String subject, Term term, String title,
+            Integer year) {
+        Course course = new Course();
+        course.setNumber(number);
+        course.setSection(section);
+        course.setSubject(subject);
+        course.setTerm(term);
+        course.setTitle(title);
+        course.setYear(year);
+        return course;
+    }
+
+    /**
+     * Helper function for asserting the equality of two courses.
+     */
+    public static void assertSimpleEquality(Course course1, Course course2) {
+        Assert.assertEquals(course1.getId(), course2.getId());
+        Assert.assertEquals(course1.getNumber(), course2.getNumber());
+        Assert.assertEquals(course1.getSection(), course2.getSection());
+        Assert.assertEquals(course1.getSubject(), course2.getSubject());
+        Assert.assertEquals(course1.getTerm(), course2.getTerm());
+        Assert.assertEquals(course1.getTitle(), course2.getTitle());
+        Assert.assertEquals(course1.getYear(), course2.getYear());
     }
 
 }
